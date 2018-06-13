930f48662f60a8c7e82e541f5739b48dc61b824f
Harlen Hobbs <hob14003@byui.edu>
2018-06-13 15:29:20 -0600

    Added basic getter/setters for Employee.java. Has problems with Rank and oddly strings or use of system.out.println()

--- src/scheduler/Employee.java
+++ src/scheduler/Employee.java
@@ -1,5 +1,71 @@
 package scheduler;
 
 public class Employee {
+    //String name;
+    int ID;
+    int phoneNumber;
+    //List <Rank> rank;
+    Availability availability;
+    int hoursScheduled;
+    int hourCap;
 
+    /*public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    } */
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID (int ID) {
+        this.ID = ID;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        //this should probably have some restraints on phoneNumber values
+        this.phoneNumber = phoneNumber;
+    }
+    /*public Rank getRank() {
+        return rank;
+    }
+
+    public Rank setRank(Rank rank) {
+        this.rank = rank;
+    } */
+
+    public Availability getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Availability availability) {
+        this.availability = availability;
+    }
+
+    public int getHoursScheduled() {
+        return hoursScheduled;
+    }
+
+    public void setHoursScheduled(int hoursScheduled) {
+        this.hoursScheduled = hoursScheduled;
+    }
+
+    public int getHourCap() {
+        return hourCap;
+    }
+
+    public void setHourCap(int hourCap) {
+        if(hourCap < 41)
+            this.hourCap = hourCap;
+        else {
+            //System.out.println("Hour cap must be 40 hrs or less.");
+        }
+    }
 }
