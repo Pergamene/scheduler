fe78a77e577cfb5907020c50c24099602ec2bb96
Harlen Hobbs <hob14003@byui.edu>
2018-06-13 14:25:53 -0600

    Time.java filled out

--- src/scheduler/Time.java
+++ src/scheduler/Time.java
@@ -1,5 +1,22 @@
 package scheduler;
 
 public class Time {
+    int startTime = 0;
+    int endTime = 0;
 
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime:
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
 }
