cbf13d50b9ebf6a59daa1ab186aecf34b83f3337
Harlen Hobbs <hob14003@byui.edu>
2018-06-15 14:32:18 -0600

    Test commit from intellij. Created shift

--- src/scheduler/Shift.java
+++ src/scheduler/Shift.java
@@ -1,5 +1,40 @@
 package scheduler;
 
 public class Shift {
+    String shiftName;
+    Rank requiredRank;
+    Time time;
+    Employee assignedEmployee;
 
+    public String getShiftName() {
+        return shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public Rank getRequiredRank() {
+        return requiredRank;
+    }
+
+    public void setRequiredRank(Rank requiredRank) {
+        this.requiredRank = requiredRank;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public Employee getAssignedEmployee() {
+        return assignedEmployee;
+    }
+
+    public void setAssignedEmployee(Employee assignedEmployee) {
+        this.assignedEmployee = assignedEmployee;
+    }
 }
