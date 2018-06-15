d016757484136947f4de8e997e145edc51acf51e
Harlen Hobbs <hob14003@byui.edu>
2018-06-15 14:14:43 -0600

    Test commit from intellij. Editted Availability.java

--- src/scheduler/Availability.java
+++ src/scheduler/Availability.java
@@ -1,5 +1,17 @@
 package scheduler;
 
-public class Availability {
+import java.util.List;
 
+public class Availability {
+    List<AvailableDay> availableDays;
+    int totalHours = 0;
+    Date ro;
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(int hours) {
+        totalHours += hours; //not sure if this was the intended idea.
+    }
 }
