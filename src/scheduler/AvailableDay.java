c3ec948983c2c0a3572ab2fd53baea14f962b876
Harlen Hobbs <hob14003@byui.edu>
2018-06-15 14:08:52 -0600

    Test commit from intellij. Editted AvailableDay.java

--- src/scheduler/AvailableDay.java
+++ src/scheduler/AvailableDay.java
@@ -1,5 +1,22 @@
 package scheduler;
 
 public class AvailableDay {
+    Day day;
+    Time time;
 
+    public Day getDay() {
+        return day;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
