d95df1babf59c97d75223dc9885471fbb4148055
Harlen Hobbs <hob14003@byui.edu>
2018-06-15 14:49:57 -0600

    Test commit from intellij. Added generate schedule

---
+++ src/scheduler/generateSchedule.java
@@ -0,0 +1,13 @@
+package scheduler;
+
+import java.util.List;
+
+public class generateSchedule {
+    List<Employee> Employees;
+    List<DayProfile> dayProfiles;
+    Schedule schedule;
+
+    public void generateSchedule() {
+        //where it all comes together.
+    }
+}
