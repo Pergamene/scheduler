2163c6e3861002b5a07e880e3b7118dd38f8978a
Harlen Hobbs <hob14003@byui.edu>
2018-06-15 14:23:04 -0600

    Test commit from intellij. Edited workProfile.java Does what Rank did in our design doc while not messing with the rank enum file name.

---
+++ src/scheduler/workProfile.java
@@ -0,0 +1,23 @@
+package scheduler;
+
+public class workProfile {
+    Rank rank;
+    Area area;
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+}
