eb61935978d4612a7a80ee164fffb486f912b32b
Pergamene <smartz373@gmail.com>
2018-07-05 09:17:08 -0600

    Added RequestTimeOff and TimeTest

---
+++ src/scheduler/TimeTest.java
@@ -0,0 +1,71 @@
+package scheduler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTest {
+//
+//    private Time t1;
+//    private Time t2;
+//
+//    @BeforeEach
+//    void setUp() {
+//        t1 = new Time("12:00 PM", "12:00 AM");
+//        t2 = new Time("01:32 PM", "11:46 PM");
+//    }
+//
+//    @Test
+//    void getStartTimeIntTest() {
+//        assertEquals(1200, t1.getStartTimeInt());
+//        assertEquals(1332, t2.getStartTimeInt());
+//    }
+//
+//    @Test
+//    void getStartTimeTest() {
+//        assertTrue(t1.getStartTime().equals("12:00 PM"));
+//        assertTrue(t2.getStartTime().equals("01:32 PM"));
+//    }
+//
+//    @Test
+//    void getEndTimeIntTest() {
+//        assertEquals(0000,t1.getEndTimeInt());
+//        assertEquals(2346, t2.getEndTimeInt());
+//    }
+//
+//    @Test
+//    void getEndTimeTest() {
+//        System.out.println(t1.getEndTime());
+//        assertTrue(t1.getEndTime().equals("12:00 AM"));
+//        assertTrue(t2.getEndTime().equals("11:46 PM"));
+//    }
+//
+//    @Test
+//    void setStartTimeTest() {
+//        t1.setStartTime("11:59 AM");
+//        t2.setStartTime("04:23 AM");
+//        assertTrue(t1.getStartTime().equals("11:59 AM"));
+//        assertTrue(t2.getStartTime().equals("04:23 AM"));
+//    }
+//
+//    @Test
+//    void setEndTimeTest() {
+//        t1.setEndTime("11:37 PM");
+//        t2.setEndTime("01:51 AM");
+//        assertTrue(t1.getEndTime().equals("11:37 PM"));
+//        assertTrue(t2.getEndTime().equals("01:51 AM"));
+//    }
+//
+//    @Test
+//    void getTotalHoursTest() {
+//        assertEquals(1200, t1.getTotalHours());
+//        assertEquals(1014, t2.getTotalHours());
+//    }
+//
+//    @Test
+//    void toStringTest() {
+//        assertTrue(t1.toString().equals("12:00 PM-12:00 AM"));
+//        assertTrue(t2.toString().equals("01:32 PM-11:46 PM"));
+//    }
+}
