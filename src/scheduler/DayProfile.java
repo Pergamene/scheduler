aa4e42cb92aade0a28d556a43f5586e6c3c3dd3b
Harlen Hobbs <hob14003@byui.edu>
2018-07-06 13:50:14 -0600

    added default constructor

--- src/scheduler/DayProfile.java
+++ src/scheduler/DayProfile.java
@@ -10,6 +10,9 @@ public class DayProfile {
     private Map<String, Shift> shifts;
     private Day day;
 
+
+    public DayProfile() {
+    }
     public DayProfile(String label, Day day) {
         this.label = label;
         this.day = day;
@@ -23,6 +26,7 @@ public class DayProfile {
     public void setLabel(String label) {
         this.label = label;
     }
+    //public void setDay(String day) {
 
     public void removeShift(String shiftName) {
         shifts.remove(shiftName);
@@ -33,6 +37,8 @@ public class DayProfile {
         //Otherwise, inform user that it is already being used.
         if(shifts.put(shift.getShiftName(), shift) != null) {
             System.out.println("Shift is already in use");
+        } else {
+            shifts.put(shift.getShiftName(), shift);
         }
     }
 
@@ -40,6 +46,8 @@ public class DayProfile {
         return shifts.values();
     }
 
+    public void setDay(Day day) {this.day = day;}
+    public Day getDay() {return day;}
     public void assignEmployee(Shift shift, Employee employee) {
         shifts.get(shift.getShiftName()).setAssignedEmployee(employee);
     }
