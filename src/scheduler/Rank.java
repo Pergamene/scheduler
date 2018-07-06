70ccdac68cc0017884f0fc0b493a557995018c48
Harlen Hobbs <hob14003@byui.edu>
2018-07-06 13:51:34 -0600

    ,

--- src/scheduler/Rank.java
+++ src/scheduler/Rank.java
@@ -1,5 +1,14 @@
 package scheduler;
 
 public enum Rank {
-    TEAM_MEMBER, TEAM_LEADER, SUPERVISOR, SHIFT_MANAGER
+    TEAM_MEMBER, TEAM_LEADER, SUPERVISOR, SHIFT_MANAGER;
+
+    /*public static Rank fromString(String text) {
+        for (Rank r : Rank.values()) {
+            if (r.toString().equalsIgnoreCase(text)) {
+                return r;
+            }
+        }
+        return null;
+    }*/
 }
