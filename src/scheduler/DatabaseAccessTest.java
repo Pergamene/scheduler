797cdeb7b95e2dc63ba2d1c0a22560ac91e55651
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 12:00:26 -0600

    JavaDoc DatabaseAccessTest

--- src/scheduler/DatabaseAccessTest.java
+++ src/scheduler/DatabaseAccessTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Simple test class to verify access to database.
+ */
 class DatabaseAccessTest {
 
     private DatabaseAccess dba;
