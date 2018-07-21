aab308dd05de882b9a8cdf682b34a208ceed9014
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 11:25:39 -0600

    JavaDoc for Schedule

--- src/scheduler/Schedule.java
+++ src/scheduler/Schedule.java
@@ -3,13 +3,24 @@ package scheduler;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Creates a map of Day and DayProfile. This makes it easier to search through days to find the appropriate profile.
+ */
 public class Schedule {
     private Map<Day, DayProfile> schedule = new HashMap<>();
 
+    /**
+     * Default constructor
+     */
     public Schedule() {
         schedule = new HashMap<Day, DayProfile>();
     }
 
+    /**
+     * Adds a day and dayProfile to the Schedule map. It checks to make sure that values have not been sent previously.
+     * @param day
+     * @param dayProfile
+     */
     public void addDayProfile(Day day, DayProfile dayProfile) {
         //This will accept the day of week that the new profile is to be created for and the name
         //that the new profile will be known as. We should verify that there are no others of the
@@ -20,6 +31,12 @@ public class Schedule {
         schedule.put(day, dayProfile);
     }
 
+    /**
+     * Adds a day(string) and dayProfile to the Schedule map. It checks to make sure that values have not been sent
+     * previously.
+     * @param day
+     * @param dayProfile
+     */
     public void addDayProfile(String day, DayProfile dayProfile) {
         //This will accept the day of week that the new profile is to be created for and the name
         //that the new profile will be known as. We should verify that there are no others of the
@@ -30,11 +47,21 @@ public class Schedule {
         schedule.put(Day.valueOf(day), dayProfile);
     }
 
+    /**
+     * Returns the dayProfile based on the day sent.
+     * @param day
+     * @return
+     */
     public DayProfile getDayProfile(Day day) {
         //This show profiles matching the sent day
         return schedule.get(day);
     }
 
+    /**
+     * Removes an entry into schedule based on day/dayProfile sent.
+     * @param day
+     * @param dayProfile
+     */
     public void removeDayProfile(Day day, DayProfile dayProfile) {
         //This should check the sent day for the sent dayProfile, if there is an instance that
         //matches, it will remove that profile (maybe an "are you sure you want to..." message
