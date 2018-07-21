75e99dc6ba6287671361824eab1bc28c7b7cf7e2
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 11:39:55 -0600

    JavaDoc for SortByCoverage

--- src/scheduler/SortByCoverage.java
+++ src/scheduler/SortByCoverage.java
@@ -2,6 +2,9 @@ package scheduler;
 
 import java.util.Comparator;
 
+/**
+ * Sorts the employee priority according to available coverage an employee can commit to
+ */
 public class SortByCoverage implements Comparator<Shift> {
     @Override
     public int compare(Shift s1, Shift s2) {
