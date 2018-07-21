fa5c4769334c8d1171e6dfb3ed5efa082b32aa92
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 10:52:15 -0600

    JavaDoc DayProfile

--- src/scheduler/DayProfile.java
+++ src/scheduler/DayProfile.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Creates a dayProfile that has a label, a map of shifts, and then day of week that the profile falls on.
+ */
 public class DayProfile {
 
     private String label;
@@ -16,22 +19,40 @@ public class DayProfile {
         shifts = new HashMap<String, Shift>();
     }
 
+    /**
+     * Default constructor of DayProfile
+     */
     public DayProfile() {
 
     }
 
+    /**
+     * Returns the label
+     * @return
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Sets the label based on the string sent.
+     * @param label
+     */
     public void setLabel(String label) {
         this.label = label;
     }
 
+    /**
+     * Removes a shift based on the shiftName String sent.
+     * @param shiftName
+     */
     public void removeShift(String shiftName) {
         shifts.remove(shiftName);
     }
 
+    /**
+     * Adds a shift to the shifts list based on the shift sent.
+     */
     public void addShift(Shift shift) {
         //sift through list of shifts. If shiftName != one of the items, add it.
         //Otherwise, inform user that it is already being used.
@@ -43,26 +64,50 @@ public class DayProfile {
         }
     }
 
+    /**
+     * Returns the collection of shifts
+     * @return
+     */
     public Collection<Shift> getShifts() {
         return shifts.values();
     }
 
+    /**
+     * Takes and employee and a shift sets the employee as being assigned to the shift.
+     * @param shift
+     * @param employee
+     */
     public void assignEmployee(Shift shift, Employee employee) {
         shifts.get(shift.getShiftName()).setAssignedEmployee(employee);
     }
 
+    /**
+     * Sets the day based on a String sent.
+     * @param s
+     */
     public void setDay(String s) {
         this.day = Day.valueOf(s);
     }
 
+    /*
+     * Currently does nothing. Previously displayed all the contents in a System.out. This code was lost.
+     */
     public void display() {
 
     }
 
+    /*
+     * Similar issue as that above.
+     * @return
+     */
     public String display2() {
         return "test";
     }
 
+    /**
+     * returns the day.
+     * @return
+     */
     public Day getDay() {
         return day;
     }
