d07ed261b64e87489b8d7b763ac26e6423d29f47
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 11:40:39 -0600

    JavaDoc for WorkProfile

--- src/scheduler/WorkProfile.java
+++ src/scheduler/WorkProfile.java
@@ -1,39 +1,76 @@
 package scheduler;
 
+/**
+ * Contains Enums Area and Rank
+ */
 public class WorkProfile {
     private Rank rank;
     private Area area;
 
+    /**
+     * Default constructor
+     */
     public WorkProfile() {
 
     }
 
+    /**
+     * Non-default constructor that sets the workProfile values based on a received Rank and Area.
+     * @param rank
+     * @param area
+     */
     public WorkProfile(Rank rank, Area area) {
         this.rank = rank;
         this.area = area;
     }
 
+    /**
+     * Non-default constructor that sets the workProfile values based on a received String(rank) and String(area).
+     * @param rank
+     * @param area
+     */
     public WorkProfile(String rank, String area) {
         this.rank = Rank.valueOf(rank);
         this.area = Area.valueOf(area);
     }
 
+    /**
+     * Returns the rank.
+     * @return
+     */
     public Rank getRank() {
         return rank;
     }
 
+    /**
+     * Sets the rank
+     * @param rank
+     */
     public void setRank(Rank rank) {
         this.rank = rank;
     }
 
+    /**
+     * Returns the Area
+     * @return
+     */
     public Area getArea() {
         return area;
     }
 
+    /**
+     * Sets the Area
+     * @param area
+     */
     public void setArea(Area area) {
         this.area = area;
     }
 
+    /**
+     * Sets the Area and Rank with String values received.
+     * @param area
+     * @param rank
+     */
     public void setWorkProfile(String area, String rank) {
         this.rank = Rank.valueOf(rank);
         this.area = Area.valueOf(area);
