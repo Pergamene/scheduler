01855f7a8b47995511c8c927cbe8e48478cbcf56
Harlen Hobbs <hob14003@byui.edu>
2018-07-21 11:25:14 -0600

    JavaDoc for Rank

--- src/scheduler/Rank.java
+++ src/scheduler/Rank.java
@@ -1,5 +1,8 @@
 package scheduler;
 
+/**
+ * Enum of Rank values. Values include: TEAM_MEMBER(0), TEAM_LEADER(1), SUPERVISOR(2), SHIFT_MANAGER(3)
+ */
 public enum Rank {
     TEAM_MEMBER(0), TEAM_LEADER(1), SUPERVISOR(2), SHIFT_MANAGER(3);
 
